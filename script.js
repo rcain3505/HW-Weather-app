5829aac45df026c1e8584dd63891b94f61b3f584
rcain3505 <114895721+rcain3505@users.noreply.github.com>
2022-11-21 23:01:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unitsmetrics`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unitsmetrics';
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
