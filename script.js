838a3394222bfed39eb585e1f2cd3305fd53620b
rcain3505 <114895721+rcain3505@users.noreply.github.com>
2022-11-24 15:22:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unitsmetrics`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metrics`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
@@ -93,12 +93,12 @@ function displayTemperature(response){
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
  
-  getForecast(response.data.coord);
+  getForecast(response.data.coords);
 }
 
 function searchCity(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
@@ -142,8 +142,8 @@ let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", handleSubmit);
+let searchform = document.querySelector("#search-form");
+searchform.addEventListener("submit", handleSubmit);
 
 
 searchCity("New York");
